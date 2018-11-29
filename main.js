ff334981ec922da7007e37a5fca834485238aeaa
nisol91 <nicolasolzi91@gmail.com>
2018-11-29 12:53:30 +0100

    3

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 // Creare un hamburger menu con l’utilizzo di Html,
 // Css e JavaScript.
 
-$('.menu-h').hide();
+// $('.menu-h').hide();
+// $('.h-m').click(function () {
+//   $('.menu-h').show()
+//   $('.nav-bar').hide()
+// });
+// $('.x-x').click(function () {
+//   $('.menu-h').hide()
+//   $('.nav-bar').show()
+// });
+
+
+//in alternativa (che in realta e la scelta piu utilizzata) mi creavo una
+//classe nel css con display flex per il menu dell hamburger e tramite jquery
+//gliela aggiungevo/toglievo per mostrarlo/nasconderlo.
+
+
 $('.h-m').click(function () {
-  $('.menu-h').show()
-  $('.nav-bar').hide()
+  $('.menu-h').addClass('attivo')
+  $('.nav-bar').addClass('disattivo')
 });
 $('.x-x').click(function () {
-  $('.menu-h').hide()
-  $('.nav-bar').show()
+  $('.menu-h').removeClass('attivo')
+  $('.nav-bar').removeClass('disattivo')
 });
